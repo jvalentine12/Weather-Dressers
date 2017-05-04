4fbbec7f03d4f83382b97feb90b7505d68a3b362
funboychris15 <pfiguereo@cristoryny.org>
2017-05-04 12:46:21 +0000

    newly added content in progress

--- Javascript/scriptpg3.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $("#next").click(function(){
-        $("#search-button2").click(function(){
-            alert("#search-button2");
-            
-        });
-        
-    });
-});
