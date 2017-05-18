edfa33047f9076ffd2e2ba39e9808d1d0139b65f
funboychris15 <pfiguereo@cristoryny.org>
2017-05-18 12:45:31 +0000

    updated project

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var temp="";
 $(document).ready(function(){
     $("#search-button").click(function(){
        
@@ -13,28 +14,66 @@ $(document).ready(function(){
             var windspeed = response.current.wind_mph;
             var humidity = response.current.humidity;
             $("#weather-results").append("<br>" + searchval + ":" + "<br>" + "Temp F: "+ temperatureF + " ,Temp C: " + temperatureC + "<br>" + "Windspeed: " + windspeed + "<br>" + "Humidity: " + humidity + "<br>");
+        
             if (temperatureF<50){
-                    var part3= "http://api.shopstyle.com/api/v2/products?";
+                    var part3= "https://api.shopstyle.com/api/v2/products?";
                     var part4= "pid=uid8969-39322563-72&";
-                    var searches= "fts=winter+boots&";
-                    var  part5= "offset=0&";
-                    var part6="limit=12";
+                    var searches= "fts=winter+boots+winter+coat+winter+pants&";
+                    var part5= "offset=0&";
+                    var part6="limit=5";
                     var URL_c= part3 + part4 + searches + part5 + part6; 
-                   
-                    $.getJSON(URL_c,function(response){
-                        
-                    }
-                alert("COLD")    
                     
-                }
-             else{
-                alert("WARM") ;
+                    $.getJSON(URL_c,function(response){
+                        console.log(response);
+                            //for (i = 0, i<response.length,i++)
+                            var P_name = response.products[0].name;
+                            var P_img = response.products[0].image.sizes.Medium.url;
+                            var P_price= response.products[0].priceLabel;
+                            var P_size= response.products[0].sizes.Medium;
+                   
+                    $("#clothing").append("<br>" + "<img src=" + P_img + "></img>");  
+                    $("#clothing").append("<br>" + P_name + "<br>" + P_size + "<br>" + P_price);
+                        
+                    });
             }
-                
-            });
-    });
+                 else {
+                    var part3= "https://api.shopstyle.com/api/v2/products?";
+                    var part4= "pid=uid8969-39322563-72&";
+                    var searches= "fts=summer+shorts+summer+shirts&";
+                    var part5= "offset=0&";
+                    var part6="limit=5";
+                    var URL_c= part3 + part4 + searches + part5 + part6; 
+                    
+                    $.getJSON(URL_c,function(response){
+                            //for (i = 0, i)
+                            var P_name = response.products[0].name;
+                            var P_img = response.products[0].image.sizes.Medium.url;
+                            var P_price= response.products[0].priceLabel;
+                            var P_size= response.products[0].sizes.Medium;
+                   
+                    $("#clothing").append("<br>" + "<img src=" + P_img + "></img>");  
+                    $("#clothing").append("<br>" + P_name + "<br>" + P_size + "<br>" + P_price);
+                 
+                   
+                    });
 
- });
+                }
+    });
+        
+});
+
+            
+            
+            
+            
+            
 // $("#search-button2").click(function(){
-//         var 
-// });
+//         alert("working");
+//         alert(temp);
+        
+                  
+         
+                
+          
+
+ 
